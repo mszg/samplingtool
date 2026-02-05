cb2ca0d7c0249741e31e1f78f8a141fd1bd0c7f4
mszg <gergo.msz2000@gmail.com>
2026-02-05 21:10:21 +0100

    1

--- sampling_app/urls.py
+++ sampling_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),                 # main page
-    path('run/', views.run_sampling, name='run_sampling'),  # ajax endpoint later
+    path('', views.home, name='home'),
+    path('run/', views.run_sampling, name='run_sampling'),
+    path('progress/<str:job_id>/', views.progress, name='progress'),
+    path('result/<str:job_id>/', views.result, name='result'),
 ]
